9fd1e2a21c4b408e29abccf92dca003c5221b094
Sabuj Dutta <112894964+freake-n@users.noreply.github.com>
2024-01-15 20:12:54 +0530

    Update pattern_22.java

--- Step 1 : Learning basics/1.2(Logical Thinking)/pattern_22.java
+++ Step 1 : Learning basics/1.2(Logical Thinking)/pattern_22.java
@@ -1,13 +1,13 @@
-public class pattern_22
-{
+public class pattern_22 {
     public static void main(String[] args) {
         
-        for(int i=1;i<=5;i++)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j);
+        int n = 4;
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+
+                System.out.print(Math.max(Math.abs(n - i), Math.abs(n - j)) + 1);
             }
+
             System.out.println();
         }
     }
