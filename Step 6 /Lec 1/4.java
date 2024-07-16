cf7418baf9740fe635779b3f5c1265d94c40677c
Sabuj Dutta <112894964+freake-n@users.noreply.github.com>
2024-07-16 20:25:23 +0530

    Create 4.java

---
+++ Step 6 /Lec 1/4.java
@@ -0,0 +1,19 @@
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        int c=0;
+        while(head!=null)
+        {
+            c++;
+            head=head.next;
+        }
+        return c;
+    }
+}
