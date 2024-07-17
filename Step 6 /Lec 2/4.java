afd38672ab3791249ee6585349caf46a4a2c1b0c
Sabuj Dutta <112894964+freake-n@users.noreply.github.com>
2024-07-17 21:10:47 +0530

    Create 4.java

---
+++ Step 6 /Lec 2/4.java
@@ -0,0 +1,39 @@
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+*/
+public static Node reverseDLL(Node  head)
+{
+    if (head == null) {
+        return head;
+    }
+    
+    Node current = head;
+    Node temp = null;
+    
+    // Swap next and prev for all nodes of the doubly linked list
+    while (current != null) {
+        temp = current.prev;
+        current.prev = current.next;
+        current.next = temp;
+        current = current.prev;  // Move to the next node (which is the previous node before swapping)
+    }
+    
+    // Before changing head, check for the cases like empty list and
+    // list with only one node
+    if (temp != null) {
+        head = temp.prev;
+    }
+    
+    return head;
+}
